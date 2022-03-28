79d46557274a10c7a5592b66eb500897d045de6f
Anastasia Osipova <asunavv0t@gmail.com>
2022-03-28 12:44:44 +1000

    create calculator aplucation

--- plusAB.py
+++ plusAB.py
@@ -3,3 +3,21 @@ def F(n):
         return F(n-1)*n
     else: return 1
 print(F(3))
+
+def str(s):
+    return s[len(s)-1]+s[1:len(s)-1]+s[0]
+
+def summa(a , b):
+    return a+b
+
+def mult(a, b):
+    return a*b
+
+a = 5
+b = 10
+s = "1234"
+print(F(a))
+print(str(s))
+print(summa(a, b))
+print(mult(a, b))
+
